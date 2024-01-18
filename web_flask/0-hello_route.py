ef6cb04976c8a19da5ffa15c8f971416154e7e62
root <root@DESKTOP-TJCQE43>
2024-01-18 23:52:00 +0200

    created teh basic files for the project and the flask app that returns Hello HBNB

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# script that starts a Flask web application:
+# web application listening on 0.0.0.0, port 5000
+# Routes:  "/"" display “Hello HBNB!”
+# option strict_slashes=False in route definition
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """ returns a message on the index """
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    """ if not imported execute the main function """
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/README.md
@@ -0,0 +1,8 @@
+This includes the implementation of teh web framework using Flask
+Each script does exactly what it describes down below
+
+0-hello_route.py:
+script that starts a Flask web application listening on 0.0.0.0, port 5000
+Routes:
+/: display “Hello HBNB!”
+NB: strict_slashes=False in route definition
