d44a86f54ea9e3ed0870119be42731acf504301b
root <root@DESKTOP-TJCQE43>
2024-01-19 00:15:39 +0200

    made PEP 8 corrections to teh python files

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" Starts a Flash Web Application """
 # script that starts a Flask web application:
 # web application listening on 0.0.0.0, port 5000
 # Routes:  "/"" display “Hello HBNB!”
@@ -7,10 +8,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_world():
     """ returns a message on the index """
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     """ if not imported execute the main function """
